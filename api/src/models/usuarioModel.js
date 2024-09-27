0ec487e0a0c274c86b67fd9347e297f8e5f1e9db
NerioJose <neriojose531@gmail.com>
2024-09-27 14:48:51 -0500

    Se deja el modelo en blanco

--- api/src/models/usuarioModel.js
+++ api/src/models/usuarioModel.js
@@ -2,28 +2,6 @@ const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {  
   sequelize.define('Usuario', {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true,
-    },
-    nombre: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    rol: {
-      type: DataTypes.ENUM('usuario', 'admin'),
-      allowNull: false,
-      defaultValue: 'usuario',
-    },
+    
   });
 };
